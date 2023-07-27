275fb9958addbb5493fff97bbd83e823a6d98af2
ShashwatRai575 <shashwatrai575@gmail.com>
2023-07-27 11:33:51 +0530

    Inheritance

---
+++ src/Java_80_Exception_and_Try_Catch_Block.java
@@ -0,0 +1,19 @@
+public class Java_80_Exception_and_Try_Catch_Block {
+        public static void main(String[] args) {
+            int a = 6000;
+            int b = 0;
+            // Without Try:
+//        int c = a / b;
+//        System.out.println("The result is " + c);
+            // With Try:
+            try {
+                int c = a / b;
+                System.out.println("The result is " + c);
+            }
+            catch(Exception e) {
+                System.out.println("We failed to divide. Reason: ");
+                System.out.println(e);
+            }
+            System.out.println("End of the program");
+        }
+    }
